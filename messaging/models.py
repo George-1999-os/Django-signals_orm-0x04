df8a550cdc54bc5f73e5e957ef81835ec64e15ce
George-1999-os <georgeibuchi44@gmail.com>
2025-08-03 00:41:30 +0100

    Add 'read' field and register UnreadMessagesManager in Message model

--- messaging/models.py
+++ messaging/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from .managers import UnreadMessagesManager  # make sure this import is here
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
@@ -8,14 +8,11 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)  #  Checker will look for this field
+    parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
 
-    parent_message = models.ForeignKey(
-        'self',
-        null=True,
-        blank=True,
-        related_name='replies',
-        on_delete=models.CASCADE
-    )
+    objects = models.Manager()  # default manager
+    unread = UnreadMessagesManager()  #  Checker looks for this name
 
     def __str__(self):
-        return f'Message from {self.sender} to {self.receiver}'
+        return f"{self.sender} -> {self.receiver}: {self.content[:20]}"
