343e712bcdf29cd41dec51e1f3c4c50560af446e
George-1999-os <georgeibuchi44@gmail.com>
2025-08-03 01:37:41 +0100

    Refactor models: add MessageHistory and Notification, update Message

--- messaging/models.py
+++ messaging/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .managers import UnreadMessagesManager  # make sure this import is here
+
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
@@ -8,11 +8,25 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
-    read = models.BooleanField(default=False)  #  Checker will look for this field
-    parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
-
-    objects = models.Manager()  # default manager
-    unread = UnreadMessagesManager()  #  Checker looks for this name
+    parent_message = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.sender} -> {self.receiver}: {self.content[:20]}"
+        return f'From {self.sender} to {self.receiver} at {self.timestamp}'
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Notification for {self.user} about message {self.message.id}'
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, related_name='history', on_delete=models.CASCADE)
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'History of message {self.message.id} edited at {self.edited_at}'
